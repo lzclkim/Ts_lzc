eed98467a872432c0687c5780603141226f99b07
lzc <liuzc@dragoninfo.com.cn>
2023-08-08 22:01:00 +0800

    13.Day01 上午 时长45：45，TS的重要性

---
+++ 01_TypeScrip语法基础/01_javaScrip类型缺失.js
@@ -0,0 +1,9 @@
+function getLength(args){
+    return args.length;
+}
+
+console.log(getLength("222222"));
+console.log(getLength([1,2,3,4,5,6,7,8,9,10]));
+
+console.log(getLength(123));
+console.log(getLength(true));
---
+++ 01_TypeScrip语法基础/02_TypeScrip类型检测.ts
@@ -0,0 +1,13 @@
+function getLength(args: { length: number }) {
+    return args.length;
+}
+
+getLength("222222")
+getLength([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+const info = {
+    length: 100
+}
+getLength("123")
+
+export {}
